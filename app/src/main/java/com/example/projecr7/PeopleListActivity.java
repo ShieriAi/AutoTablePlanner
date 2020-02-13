452c26b7c90da92ddf37fa99e4907ef58b7c6c96
ShieriAi <xuchen.he@student.manchester.ac.uk>
2020-02-13 15:40:17 +0000

    fab added

--- app/src/main/java/com/example/projecr7/PeopleListActivity.java
+++ app/src/main/java/com/example/projecr7/PeopleListActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.projecr7.database.DatabaseClient;
@@ -26,6 +28,12 @@ public class PeopleListActivity extends AppCompatActivity {
     private List<Person> people;
     private int dinnerId;
 
+    private FloatingActionButton fab_main, fab1_person, fab2_couple;
+    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
+    TextView textview_person, textview_couple;
+    Boolean isOpen = false;
+
+
     private onClickInterface mOnClickInterface;
 
     @Override
@@ -36,7 +44,7 @@ public class PeopleListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         dinnerId = intent.getIntExtra(MainActivity.EXTRA_INDEX, 0);
 
-        peopleRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view_people_list);
+        peopleRecyclerView = findViewById(R.id.my_recycler_view_people_list);
         peopleRecyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         peopleRecyclerView.setLayoutManager(layoutManager);
@@ -56,10 +64,51 @@ public class PeopleListActivity extends AppCompatActivity {
         TextView textview = findViewById(R.id.textViewDinnerName_peoplelist);
         textview.setText(DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().dinnerDao().loadSingleById(dinnerId).toString());
 
-        FloatingActionButton fab = findViewById(R.id.fab_people);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = findViewById(R.id.fab_people);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(PeopleListActivity.this,AddPersonActivity.class);
+//                intent.putExtra(MainActivity.EXTRA_INDEX, dinnerId);
+//                startActivity(intent);
+//            }
+//        });
+
+        fab_main = findViewById(R.id.fab_main_people);
+        fab1_person = findViewById(R.id.fab_people);
+        fab2_couple = findViewById(R.id.fab_add_couple);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_clock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_clock);
+        fab_anticlock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_anticlock);
+
+        textview_person = findViewById(R.id.textview_person);
+        textview_couple = findViewById(R.id.textview_couple);
+
+        fab_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (isOpen) {
+                    fabClose();
+                } else {
+                    fabOpen();
+                }
+
+            }
+        });
+
+
+        fab2_couple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        fab1_person.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabClose();
                 Intent intent = new Intent(PeopleListActivity.this,AddPersonActivity.class);
                 intent.putExtra(MainActivity.EXTRA_INDEX, dinnerId);
                 startActivity(intent);
@@ -70,7 +119,7 @@ public class PeopleListActivity extends AppCompatActivity {
 
     public void updatePeopleList(){
         people = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().personDao().loadAllByDinner(dinnerId);
-        Person[] peopleArray;;
+        Person[] peopleArray;
         if(people.size() != 0){
             peopleArray =  new Person[people.size()];
             for(int i = 0; i < people.size(); i++){
@@ -79,5 +128,27 @@ public class PeopleListActivity extends AppCompatActivity {
             mPeopleAdapter = new MyPeopleListAdapter(peopleArray, mOnClickInterface);
             peopleRecyclerView.setAdapter(mPeopleAdapter);
         }
+    }
+
+    private void fabOpen(){
+        textview_person.setVisibility(View.VISIBLE);
+        textview_couple.setVisibility(View.VISIBLE);
+        fab2_couple.startAnimation(fab_open);
+        fab1_person.startAnimation(fab_open);
+        fab_main.startAnimation(fab_clock);
+        fab2_couple.setClickable(true);
+        fab1_person.setClickable(true);
+        isOpen = true;
+    }
+
+    private void fabClose(){
+        textview_person.setVisibility(View.INVISIBLE);
+        textview_couple.setVisibility(View.INVISIBLE);
+        fab2_couple.startAnimation(fab_close);
+        fab1_person.startAnimation(fab_close);
+        fab_main.startAnimation(fab_anticlock);
+        fab2_couple.setClickable(false);
+        fab1_person.setClickable(false);
+        isOpen = false;
     }
 }
